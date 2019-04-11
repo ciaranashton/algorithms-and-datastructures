ff57d0b4541e306cd5c61b65ad373f4d911b7ad5
Ciaran Ashton <ciaranashton@me.com>
2019-04-11 16:21:57 +0100

    Linked list

---
+++ test/linkedList.test.js
@@ -0,0 +1,18 @@
+const createLinkedList = require('../LinkedList');
+const { expect } = require('chai');
+
+describe('Linked List', () => {
+  it('plan one', () => {
+    const list = createLinkedList();
+    const values = ['a', 'b', 'c', 'd', 'e', 'f'];
+    const nodes = values.map(val => list.push(val));
+
+    expect(list.print()).to.equal('a => b => c => d => e => f');
+
+    list.pop();
+    expect(list.print()).to.equal('a => b => c => d => e');
+
+    expect(list.get(2).value).to.equal('c')
+    expect(list.isEmpty()).to.be.false;
+  });
+});
